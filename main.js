49911627da3d931157f70ad36c6a41f01a07d910
platiins <valerijaaivanovaa@gmail.com>
2024-06-18 00:47:48 +0300

    adding fifth page layout

--- main.js
+++ main.js
@@ -6,4 +6,25 @@ $(() => {
   });
 });
 
-console.log("hey");
+let map;
+
+async function initMap() {
+  const position = { lat: 40.680002, lng: -73.949997 };
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+  map = new Map(document.getElementById("map"), {
+    zoom: 13,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  });
+
+  // The marker, positioned at Uluru
+  const marker = new AdvancedMarkerElement({
+    map: map,
+    position: position,
+    title: "Brooklyn",
+  });
+}
+
+initMap();
